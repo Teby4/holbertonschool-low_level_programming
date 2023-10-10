d4160fc2df9690c5f4e8aa36ea446a2bfe217288
Teby4 <felipeoliveramantero@gmail.com>
2023-10-10 06:49:03 -0700

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - print text followed by new line
+ * Return: 0
+ */
+
+int main(void){
+printf "with proper grammar, but the outcome is a piece of art,";
+puts ("\n");
+return (0);
+}
