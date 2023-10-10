ef2303e9ea9edb26e0129febddf870c471701ffb
Teby4 <felipeoliveramantero@gmail.com>
2023-10-10 09:19:03 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf ("Size of a char:%d\n", sizeof char);
+	printf ("Size of an int:%d\n", sizeof int);
+	printf ("Size of a long int:%d\n", sizeof long);
+	printf ("Size of a long long int:%d\n", sizeof long double);
+	printf ("Size of a float:%d\n", sizeof float);
+	
+	return (0);
+}
