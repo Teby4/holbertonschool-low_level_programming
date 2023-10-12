5ce719084eb459cbfcaec63a5cb959a8d360bcd1
Teby4 <felipeoliveramantero@gmail.com>
2023-10-12 10:33:27 -0700

    1-last_digit.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,11 @@ int main(void)
 {
 	char alp;
 
-	for (alp = 'a'; alp <= 'z'; ++alp)
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
 	putchar("%c", alp);
-	putchar("\n")
+	}
+	putchar("\n");
 	return(0);
 
 }
