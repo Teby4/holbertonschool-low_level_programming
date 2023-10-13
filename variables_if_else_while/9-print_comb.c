73ed569287f96b43cebe8d07a1b4552d25a59b21
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 06:57:06 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - print base 10 number separated by a ,
+ * Return:0
+ */
+
+int main(void)
+{
+int p;
+for (p = '0'; p <= '9'; p++)
+{
+putchar(p)
+if (p < 9)
+{
+putchar(',')
+putchar(' ')
+}
+}
+putchar('\n')
+}
