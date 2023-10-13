7c47e06105b355e496e0d920bcb59ca7b94744f7
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 05:40:11 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+        char alp;
+
+        for (alp = 'a'; alp <= 'z'; alp++; alp != 'q'; alp != 'e')
+        {
+        putchar(alp);
+        }
+        putchar('\n');
+        return (0);
+
+}
