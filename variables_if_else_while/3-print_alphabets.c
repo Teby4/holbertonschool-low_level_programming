075fa6c0c464b81519b2ec61ad3c01ebc690a36c
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 05:30:45 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+        char alp;
+	char AZ;
+
+        for (alp = 'a'; alp <= 'z'; alp++)
+        {
+        putchar(alp);
+        }
+	if (alp = 'z'; AZ = 'A'; AZ <= 'Z'; AZ++)
+	{
+	putchar(AZ);
+	}
+        putchar('\n');
+        return (0);
+
+}
