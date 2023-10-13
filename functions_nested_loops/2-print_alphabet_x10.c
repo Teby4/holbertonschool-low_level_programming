29cd549dca5e9e83e62617776176badcf46247da
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 09:14:29 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void print_alphabet_x10(void);
+{
+	char abc;
+	int ten;
+
+	for(ten = 0;ten <= 10; ten++)
+	{
+		for (abc = 'a'; abc <= 'z'; abc++)
+		{
+			_putchar(abc);
+		}
+	}
+	_putchar('\n')
+}
