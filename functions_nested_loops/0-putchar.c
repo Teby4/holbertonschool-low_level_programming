5cbbef252ea01ea091009375ffdde2a626caf19a
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 07:38:28 -0700

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
 #include<stdio.h>
+#include<main.h>
+
+/**
+ * main - _putchar
+ * Return: 0 
+ */
 
 int main(void)
 {
-	_putchar('_')
-	_putchar('p')
-	_putchar('u')
-	-putchar('t')
-	-putchar('c')
-	-putchar('h')
-	-putchar('a')
-	_putchar('r')
-	_putchar('\n')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	-putchar('t');
+	-putchar('c');
+	-putchar('h');
+	-putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	
 	return (0);
 }
