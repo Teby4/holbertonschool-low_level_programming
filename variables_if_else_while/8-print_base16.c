a2e14dd85388bea31d80b6d456aea4c0519ea09b
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 06:33:03 -0700

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
 /**
- * main - print base 10
- * Return: 0
- */
+* main - print base 10
+* Return: 0
+*/
 
 int main(void)
 {
-        char t;
+char t;
 
-        for (t = 48; t <= 57; t++)
-        {
-                putchar(t);
-        }
-	for (t = 97; t <= 102; t++)
-	{
-		putchar(t);
-	}
+for (t = 48; t <= 57; t++)
+{
+putchar(t);
+}
+for (t = 97; t <= 102; t++)
+{
+putchar(t);
+}
 
-        putchar('\n');
-        return (0);
+putchar('\n');
+return (0);
 }
