b8866d1279ae75074d7cb095fb346ccd8892a200
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 09:28:21 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * _islower - check if the letter is lowercase
+ * Return: 
+ */
+
+int _islower(int c)
+{
+	int il;
+
+	if (islower(il) != 0)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
