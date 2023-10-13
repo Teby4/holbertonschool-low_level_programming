62c0b524776643050db84644f7bd83c4659fd2f6
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 06:09:10 -0700

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
 /**
- * main - print base 10
- * Return: 0
- */
+* main - print base 10
+* Return: 0
+*/
 
 int main(void)
 {
-        int t;
+int t;
 
-        for (t = '0'; t <= '9'; t++)
-        {
-                putchar(t);
-        }
+for (t = '0'; t <= '9'; t++)
+{
+putchar(t);
+}
 
-        putchar('\n');
-        return (0);
+putchar('\n');
+return (0);
 }
