c3f7b3907dc98f0cbccb0d54b54dafa1b04c3b93
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 05:37:40 -0700

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - print alphabet
- * Return: 0
- */
+* main - print alphabet
+* Return: 0
+*/
 
 int main(void)
 {
-        char alp;
-	char AZ;
+char alp;
+char AZ;
 
-        for (alp = 'a'; alp <= 'z'; alp++)
-        {
-        putchar(alp);
-        }
-	for (AZ = 'A'; AZ <= 'Z'; AZ++)
-	{
-	putchar(AZ);
-	}
-        putchar('\n');
-        return (0);
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+putchar(alp);
+}
+for (AZ = 'A'; AZ <= 'Z'; AZ++)
+{
+putchar(AZ);
+}
+putchar('\n');
+return (0);
 
 }
