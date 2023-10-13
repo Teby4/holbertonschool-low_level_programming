6f75090ae3392fe65509fe9fbabb8753cc06163b
Teby4 <felipeoliveramantero@gmail.com>
2023-10-13 05:58:46 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - print base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	char t;
+
+	for (t = '1'; t <= '9'; t++)
+	{
+		putchar(t)
+	}
+	
+	putchar(\n)
+	return(0);
+
+}
