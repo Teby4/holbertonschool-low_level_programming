fae27fb46bc61cc966f3b54289013a145359a83d
Teby4 <felipeoliveramantero@gmail.com>
2023-10-15 09:56:49 -0700

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,13 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
-	return (n);
+	int last;
+	last = n % 10 ;
+
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
