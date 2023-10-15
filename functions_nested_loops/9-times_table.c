0ff3cfc1c3b88d5e2840ffbe0d72c2310083b840
Teby4 <felipeoliveramantero@gmail.com>
2023-10-15 13:37:03 -0700

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - print the times table
+ * Return: ...
+ *
+ *
+ */
+
+void times_table(void)
+{
+	int a, b;
+
+	for (a = '0';b >= '0';)
+	{
+	_putchar(a * b)
+	_putchar(44)
+	_putchar(255)
+	_putchar(255)
+	_putchar('\n')
+	}
+
+
+
+
+}
