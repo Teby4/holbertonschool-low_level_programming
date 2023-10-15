7f7bb2e96041e4c69daeb3b684ab6a529763ac9f
Teby4 <felipeoliveramantero@gmail.com>
2023-10-15 13:38:36 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,12 +12,12 @@ void times_table(void)
 {
 	int a, b;
 
-	for (a = '0';b >= '0')
+	for (a = '0';b >= '0';)
 	{
 	_putchar(a * b);
 	_putchar(44);
-	_putchar(255);
-	_putchar(255);
+	_putchar(' ');
+	_putchar(' ');
 	_putchar('\n');
 	}
 
