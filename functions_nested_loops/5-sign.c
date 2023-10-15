84edc1b226775b8425b818bdc7b5b6cd14aa38e6
Teby4 <felipeoliveramantero@gmail.com>
2023-10-15 08:23:23 -0700

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,18 +12,18 @@ int print_sign(int n)
 {
 	if (n >= 1)
 	{
-	return (1);
 	printf ("+");
+	return (1);
 	}
 	else if (n <= -1)
 	{
-	return(-1);
 	printf ("-");
+	return(-1);
 	}
 	else
 	{
-	return(0);	
 	printf ("0");
+	return(0);	
 	}
 
 
