65aa700687af80cf82676ff1f5d1d65d76695c40
Teby4 <felipeoliveramantero@gmail.com>
2023-10-16 06:02:35 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiply 2 integrers
+ * Return: result
+ * Description:
+ * @a: first number
+ * @b: second number
+ * @r: result 
+ */
+
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+	return (r);
+
+}
