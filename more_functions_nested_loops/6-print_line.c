e736d935dcda8d0f20deb0aea1be538239508390
Teby4 <felipeoliveramantero@gmail.com>
2023-10-16 08:37:15 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ * @n:
+ */
+
+void print_line(int n)
+{
+	int s;
+
+	for (s = 0; s > n; s++)
+	{
+		if (n > 0)
+		{
+		_putchar('_')
+		}
+
+	}	
+	_putchar('\n')
+
+}
