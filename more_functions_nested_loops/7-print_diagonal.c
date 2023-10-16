051205d730a3eb846c0cb03472363f9ed72387e9
Teby4 <felipeoliveramantero@gmail.com>
2023-10-16 10:14:19 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * 
+ *
+ *
+ *
+ */
+
+void print_diagonal(int n)
+
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		if (n > 0)
+		{
+		_putchar(' ');
+		}
+
+	_putchar(92);
+	_putchar('\n')
+	}
