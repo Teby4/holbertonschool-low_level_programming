b283d15c8b44c58a1c5e479f6a66c025692e9dde
Teby4 <felipeoliveramantero@gmail.com>
2023-10-16 06:12:47 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers 1 to 9 
+ * Return:
+ * Description: 
+ */
+
+void print_numbers(void)
+{
+	int o;
+
+	for (o >= 0; o <= 9; o++)
+	{
+	printf ("%d", o)
+	}
+	_putchar('\n')
+
+}
