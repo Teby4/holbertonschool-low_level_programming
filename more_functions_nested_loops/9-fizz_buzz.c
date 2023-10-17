683665cce042258a345211a5e504b555d3135822
Teby4 <felipeoliveramantero@gmail.com>
2023-10-17 06:35:44 -0700

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,25 +13,25 @@ int main(void)
 
 	for(n = 1; n <= 100; n++)
 	{
-	
-		printf ("%d ", n)
-	
+
+		printf ("%d ", n);
+
 			if (n % 3 == 0)
 			{
-			printf ("Fizz ")
+			printf ("Fizz ");
 			}
 
 			if (n % 5 == 0)
 			{
-			printf ("Buzz ")
+			printf ("Buzz ");
 			}
 
 			else if (n % 3 == 0 && n % 5 == 0)
 			{
-			printf ("FizzBuzz ")
+			printf ("FizzBuzz ");
 			}
-	
+
 	}
-	
-	printf ("\n")
+
+	printf ("\n");
 }
