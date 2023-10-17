39de6b58d4dab69abd3ffa2593b745e9daf58527
Teby4 <felipeoliveramantero@gmail.com>
2023-10-17 10:28:39 -0700

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 				_putchar(a * b / 10 + '0');
 				_putchar(a * b % 10 + '0');
 			}
-			else if (a * b < 9)
+			else if (a * b <= 9)
 			{
 			_putchar(' ');
 			_putchar(a * b + '0');
