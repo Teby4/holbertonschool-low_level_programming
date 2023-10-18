b973eaa4d4e0d03677b1d9f14c62091e6c178a10
Teby4 <felipeoliveramantero@gmail.com>
2023-10-18 05:44:05 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  takes a pointer to a int and updates the value
+ * return: 0
+ * Description:
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	*p = 98;
+	n = *p
+
+}
