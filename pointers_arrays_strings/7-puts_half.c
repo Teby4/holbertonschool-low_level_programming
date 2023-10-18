069952f167d5e442b93b1470914aa605281812b8
Teby4 <felipeoliveramantero@gmail.com>
2023-10-18 09:58:37 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - print half a string
+ * Return:
+ * Description:
+ * @str: string
+ */
+
+
+void puts_half(char *str)
+{
+	int j;
+	int cont;
+	char *ero;
+
+	ero = str;
+
+	cont = 0;
+	while (*str != '\0')
+	{
+	s++;
+	cont++;
+	}
+	j = cont/ 2
+	str[j];
+	str = ero;
+	while (str[j] != '\0')
+	{
+	_putchar(str[j]);
+	j++;
+	}
+
+	_putchar('\n');
+}
