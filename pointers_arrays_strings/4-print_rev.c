5dca18de48f55d1b61eb38bc25ceeb4addd5dd35
Teby4 <felipeoliveramantero@gmail.com>
2023-10-18 08:23:43 -0700

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@
  */
 
 void print_rev(char *s)
-{
+{	
 	char *j;
 	
 	j = s;
@@ -21,7 +21,7 @@ void print_rev(char *s)
 		s--;
 		while (s >= j)
 		{
-		_putchar(s)
+		_putchar(s);
 		s--;
 		}
 	_putchar('\n')
