62e99ff16f82f3f5d75327513160aa5d9576ca35
Teby4 <felipeoliveramantero@gmail.com>
2023-10-18 09:28:03 -0700

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - string
+ * Return:
+ * Description:
+ * @s: string
+ */
+
+void rev_string(char *s)
+{
+	char *j;
+	char temp;
+
+	j = s;
+
+		while (*s != '\0')
+		{
+		s++;
+		}
+		s--;
+		while (temp < s)
+		{
+		temp = *j;
+		*j = *s;
+		*s = temp;
+		j++;
+		s--;
+
+		}
+
+	_putchar('\n');
+}
