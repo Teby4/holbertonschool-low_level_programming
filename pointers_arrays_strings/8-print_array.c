400f709ae77b362ec3b7e18a2fe6c3ed93dfa603
Teby4 <felipeoliveramantero@gmail.com>
2023-10-18 10:32:32 -0700

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - 
+ * Return:
+ * Description:
+ * @a - array
+ * @n - array
+ */
+
+void print_array(int *a, int n)
+{
+	int j;
+	
+	j = 0;
+	while ( j != n)
+	{
+	printf("*a[j]");
+	if (j != n - 1)
+	{
+		printf(", ")
+	}
+	j++;
+	
+	}
+
+}
