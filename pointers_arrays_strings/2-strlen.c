59c990fa1702850e3316d895e3c001113299ffd7
Teby4 <felipeoliveramantero@gmail.com>
2023-10-18 07:27:07 -0700

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return the lenght of a string
+ * Return: lengt of a string
+ * Description:
+ * @s: string
+ */
+
+
+int _strlen(char *s)
+{
+	int b;
+	
+	b = 0;
+
+		while(*s != NULL)
+		{
+		b++
+		}
+
+	return (b);
+
+}
