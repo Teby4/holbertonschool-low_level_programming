a88610ea800f1f1885886dd1249041eeb3c646de
Teby4 <felipeoliveramantero@gmail.com>
2023-10-19 10:03:50 -0700

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,31 +2,31 @@
 #include <stdio.h>
 
 /**
- * _strcat - apend strings
- * Return: dest
- * Description:
- * @dest: destination
- * @src: source
- */
+* _strcat - apend strings
+* Return: dest
+* Description:
+* @dest: destination
+* @src: source
+*/
 
 char *_strcat(char *dest, char *src)
 {
-	char *j;
+char *j;
 
-	j = dest;
+j = dest;
 
-    while (*dest != '\0')
-    {
-        dest++;
-    }
+while (*dest != '\0')
+{
+dest++;
+}
 
-    while (*src != '\0')
-    {
-    *dest = *src;
-    dest++;
-    src++;
-    }
-    dest = j;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+dest = j;
 
-    return (dest);
+return (dest);
 }
