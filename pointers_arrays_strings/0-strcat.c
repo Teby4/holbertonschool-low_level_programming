46be6e1483bd7d1314dd5bdfcbb7a6f2e7d07bfc
Teby4 <felipeoliveramantero@gmail.com>
2023-10-19 13:06:03 -0300

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,7 +13,7 @@ char *_strcat(char *dest, char *src)
 {
     while (*dest != '\0')
     {       
-    *dest++;
+    dest++;
     }
 
     while (*src != '\0')
@@ -22,4 +22,6 @@ char *_strcat(char *dest, char *src)
     dest++;
     src++;
     }
+    
+    return (dest);
 }
