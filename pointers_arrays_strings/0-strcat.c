718dbda400b4638330df18856ff1955513b43080
Teby4 <felipeoliveramantero@gmail.com>
2023-10-19 13:00:31 -0300

    .\0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - apend strings
+ * Return: dest
+ * Description:
+ * @dest: destination
+ * @src: source
+ * */
+
+char *_strcat(char *dest, char *src)
+{
+    while (*dest != '\0')
+    {       
+    *dest++;
+    }
+
+    while (*src != '\0')
+    {
+    *dest = *src;
+    dest++;
+    src++;
+    }
+}
