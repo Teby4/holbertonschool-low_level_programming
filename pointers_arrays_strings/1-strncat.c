a46fd0af7e1044f3254130e9194f5d8adad649de
Teby4 <felipeoliveramantero@gmail.com>
2023-10-19 13:01:35 -0300

    .\1-strncat.c

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *strncat - 
+ * Return: dest
+ * Description:
+ * @dest: destination 
+ * @src: source
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+    
+
+
+
+
+
+}
