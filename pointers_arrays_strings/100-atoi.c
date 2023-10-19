34013e46bf819a44f0f5312445001c4e6cb01578
Teby4 <felipeoliveramantero@gmail.com>
2023-10-19 06:38:05 -0700

    100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copy the string
+ * Return: j
+ * Description:
+ * @s: source
+ */
+
+int _atoi(char *s)
+{
+	int j, n;
+
+	j = 0;
+	n = 0;
+
+	while (*s != '\0')
+	{
+		if (*s > '0')
+		{
+		j * 10 + *s - '0';
+		}
+
+		else if (*s == '-')
+		{
+		n++;
+		}
+		if (n % 2 != 0)
+		{
+		j = -j;
+		}
+	s++;
+	}
+
+	return (j);
+}
