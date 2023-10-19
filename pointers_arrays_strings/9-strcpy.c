6da47109df940232178bc4c3d50ad57190dfa50d
Teby4 <felipeoliveramantero@gmail.com>
2023-10-19 05:38:58 -0700

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copy the string
+ * Return:
+ * Description:
+ * @dest: destination
+ * @src: source
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+	dest = &src;
+	_putchar(dest);
+	src++;
+	}
+
+}
