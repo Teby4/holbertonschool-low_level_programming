cb72560a24995409532ae50a35c3a035abe8bda5
Teby4 <felipeoliveramantero@gmail.com>
2023-10-19 10:22:09 -0700

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -7,14 +7,29 @@
  * Description:
  * @dest: destination 
  * @src: source
+ * i@: n
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    
+	n = 0;
+	while (*dest != '\0')
+	{
+	dest++;
+	}
 
+	while (*src != '\0')
+	{
+		if (*src >= n)
+		{
+		break;
+		}
 
+	*dest = *src;
+	dest++;
+	src++;
+	n++;
+	}
 
-
-
+	return (dest);   
 }
