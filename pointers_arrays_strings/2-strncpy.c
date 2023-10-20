912d5ed30c7979c0521cd74ee8bfe3a52c129d7c
Teby4 <felipeoliveramantero@gmail.com>
2023-10-20 05:56:08 -0700

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,6 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	char *j;
-	int h;
 
 	j = dest;
 
@@ -20,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		if (n <= 0)
 		{
-		dest = '\0'
+		dest = '\0';
 		break;
 		}
 
