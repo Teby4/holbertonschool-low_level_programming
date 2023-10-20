c472c8ea23af43ad3a70b1cdcfbb4ba8ce7941df
Teby4 <felipeoliveramantero@gmail.com>
2023-10-20 07:38:44 -0700

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -14,11 +14,11 @@ void reverse_array(int *a, int n)
 	int temp;
 
 	i = 0;
-	while (i < n)
+	while (i < n - 1)
 	{
 	temp = a[i];
-	a[i] = a[n];
-	a[n] = temp;
+	a[i] = a[n - 1];
+	a[n - 1] = temp;
 	i++;
 	n--;
 	}
