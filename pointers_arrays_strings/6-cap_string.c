e3a12b5cc4f3486e83e0358e0bf86be1fa55fb3a
Teby4 <felipeoliveramantero@gmail.com>
2023-10-25 06:35:34 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,42 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * Return: string capitalized
- * Description:
- * @p: string
- */
+* cap_string - capitalizes all words of a string
+* Return: string capitalized
+* Description:
+* @p: string
+*/
 
 char *cap_string(char *p)
 {
-	char *j = p;
+char *j = p;
 
-	if (*p >= 'a' && *p <= 'z')
-        {
-       *p = *p -32;
-	}
-	while (*p != '\0')
-	{
-		if (*p == ' ' || *p == ',' || *p == ';' || *p == '.' || *p == '!' || 
-		*p == '?' || *p == '"' || *p == '(' || *p == ')' || *p == '{' || *p == '}' || *p == '	' || *p == '\n')
-		{
-		p++;
-			while (*p == ' ' || *p == ',' || *p == ';' || *p == '.' || *p == '!' || 
-				*p == '?' || *p == '"' || *p == '(' || *p == ')' || *p == '{' || *p == '}' || *p == '	' || *p =='\n')
-			{
-			p++;
-			}
+if (*p >= 'a' && *p <= 'z')
+{
+*p = *p -32;
+}
+while (*p != '\0')
+{
+if (*p == ' ' || *p == ',' || *p == ';' || *p == '.' || *p == '!' || 
+*p == '?' || *p == '"' || *p == '(' || *p == ')' || *p == '{' ||
+*p == '}' || *p == '	' || *p == '\n')
+{
+p++;
+while (*p == ' ' || *p == ',' || *p == ';' || *p == '.' || *p == '!' || 
+*p == '?' || *p == '"' || *p == '(' || *p == ')' || *p == '{' ||
+*p == '}' || *p == '	' || *p =='\n')
+{
+p++;
+}
 
-		        if (*p >= 'a' && *p <= 'z')
-                        {
-                        *p = *p -32;
-                        }
-		}
-	
-	p++;
-	}
-       	p = j;
-	return (p);
+if (*p >= 'a' && *p <= 'z')
+{
+*p = *p -32;
+}
+}
+
+p++;
+}
+p = j;
+return (p);
 }
