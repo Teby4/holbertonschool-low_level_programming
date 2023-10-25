050d4edf8030d91be5de12912eb677cc7b64e796
Teby4 <felipeoliveramantero@gmail.com>
2023-10-25 07:51:31 -0700

    1-memcpy.c

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -4,16 +4,16 @@
  * _memcpy - cpy memory
  * Return: dest
  * Description:
- * @dest:
- * @src:
- * @n:
+ * @dest: destination
+ * @src: source
+ * @n: numero
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int j = 0;
 
-	for(; j < n; j++)
+	for (; j < n; j++)
 	{
 		dest[j] = src[j];
 	}
