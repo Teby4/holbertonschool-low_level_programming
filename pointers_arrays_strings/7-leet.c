02ab363d1fc8ceac300695b84f6b5c4f8ad29003
Teby4 <felipeoliveramantero@gmail.com>
2023-10-25 07:06:17 -0700

    7-leet.c

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * leet - encode the string
+ * Return: p
+ * description:
+ * @p: string
+ */
+
+char *leet(char *p)
+{
+	char *j = p;
+	int cont, cont2;
+	char a['a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'];
+	char b['4', '4', '3', '3', '0', '0', '7', '7', '1', '1'];
+	
+	cont = 0;
+	cont2 = 0;
+
+	while (*p != '\0')
+	{
+		for (cont == '0'; cont < 10; cont++;)
+		{
+			if (*p == a[cont])
+			{
+				*p = b[cont];
+			}
+		}
+
+	cont2++;
+	p++;
+	}
+	
+
+	p = j;
+}
