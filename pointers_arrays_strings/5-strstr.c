e66dd1d83a491650dd08cb8d084f1b29295e1889
Teby4 <felipeoliveramantero@gmail.com>
2023-10-27 06:06:03 -0700

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * Return: pointer
+ * Description:
+ * @haystack: str
+ * @needle: str
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}
