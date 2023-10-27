6b5e256f0ad17a56b3f643c15eb527d49a486c9a
Teby4 <felipeoliveramantero@gmail.com>
2023-10-27 06:21:41 -0700

    7-print_chessboard.c

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+void print_chessboard(char (*a)[8])
+/**
+ * print_chessboard - ME QUIERO MORIR !!!!
+ * Return:
+ * Description:
+ * @a: array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int cont, cont2;
+
+	for (cont = 0; cont < 8; cont++;)
+	{
+		for (cont2 = 0; con2 < 8; cont ++)
+		{
+		_putchar(a[cont][cont2])
+		}
+	_putchar('\n')
+	}
+}
