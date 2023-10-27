2be7c38d96a6c1f99c0cfc314edd8e0299f7f483
Teby4 <felipeoliveramantero@gmail.com>
2023-10-27 05:56:27 -0700

    4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates first occurance
+ * Return:
+ * Description:
+ * @s: string
+ * @accept: string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	j = 0;
+
+	while (*s != '\0')
+	{
+	if (*s == *accept)
+	{
+	j = 1;
+	return (accept)
+	}
+	s++;
+	}
+
+	if (j == 0)
+	{
+	return ('\0')
+	}
+}
