50e16907b6e5e4a2f479e9492dbd2c690293c262
Teby4 <felipeoliveramantero@gmail.com>
2023-10-27 08:41:47 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,20 +2,23 @@
 
 /**
  * _strlen_recursion - len recursion
- * Return:
+ * Return: lenght
  * Description:
  * @s: string
  */
 
 int _strlen_recursion(char *s)
 {
+	char *j
+	
+	j = s;
 
 	if (*s != '\0')
 	{
 	s++;
 	return (_strlen_recursion((s) + 1));
 	}
-	else
+	if (j == '\0')
 	{
 	return (0);
 	}
