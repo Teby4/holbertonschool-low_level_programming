4a0095509a7585d6c6440b595389594b297f38f9
Teby4 <felipeoliveramantero@gmail.com>
2023-10-27 06:26:43 -0700

    7-p

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -11,13 +11,13 @@ void print_chessboard(char (*a)[8])
 {
 	int cont, cont2;
 
-	for (cont = 0; cont < 8; cont++;)
+	for (cont = 0; cont < 8; cont++)
 	{
 		for (cont2 = 0; cont2 < 8; cont2++)
 		{
-		_putchar(a[cont][cont2]);
+			_putchar(a[cont][cont2]);
 		}
 
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
