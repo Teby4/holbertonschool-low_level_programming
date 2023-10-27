2def45a24381c317425d62b39de7ceb9706ec6c6
Teby4 <felipeoliveramantero@gmail.com>
2023-10-27 09:30:23 -0700

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -14,9 +14,10 @@ int _pow_recursion(int x, int y)
 	{
 		return (0);
 	}
-	else if (y < 0)
+	if (y < 0)
 	{
 		return (-1);
 	}
-return (x * _pow_recursion(x, y - 1));
+
+	return (x * _pow_recursion(x, y - 1));
 }
