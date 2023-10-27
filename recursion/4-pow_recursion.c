0b1b24f87cf434c0dea6acdd8ae1cb089819cf45
Teby4 <felipeoliveramantero@gmail.com>
2023-10-27 09:08:54 -0700

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - recursion
+ * Return:
+ * Description:
+ * @x: number
+ * @y: number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return(1);
+	}
+	if (x <= 1)
+	{
+		return (_power_recursion(n * y);
+	}
+}
