f35ec41febc7038591ea182a487a42dc4051ef0a
Teby4 <felipeoliveramantero@gmail.com>
2023-10-27 08:57:02 -0700

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - return factorial of given number
+ * Return: -1 0 1
+ * Description:
+ * @n: numer
+ */
+
+
+
+int factorial(int n)
+{
+
+	if(i <= 1) 
+	{
+		return 1;
+	}
+	
+		return i * factorial(i - 1);
+}
