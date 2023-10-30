210226de420f9447322e9adb31ba55ca9f6e0a4a
Teby4 <felipeoliveramantero@gmail.com>
2023-10-30 15:02:42 -0700

    4-pow_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return square root
+ * Return: square root
+ * Description:
+ * @n: number given
+ */
+
+int _sqrt_recursion(int n)
+{
+	int y;
+
+
+}
