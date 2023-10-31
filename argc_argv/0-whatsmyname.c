fb722927d0326a6c4ff9a1082f23e9f5f34b4308
Teby4 <felipeoliveramantero@gmail.com>
2023-10-31 06:28:04 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - main function
+ * Return: 0
+ * Description:
+ * @argc: argc
+ * @argv: argv
+ */
+
 int main(int argc, char* argv[])
 {
 	printf("%s", argv[0]);
