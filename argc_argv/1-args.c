b61716488316b04239fc0b54c7cbaed659ac57e2
Teby4 <felipeoliveramantero@gmail.com>
2023-10-31 08:36:24 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ * Description:
+ * @argc: argc
+ * @argv: argv
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
