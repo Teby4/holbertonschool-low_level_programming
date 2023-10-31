5202371e5701f60c468b10fc18feafbd48ff3b48
Teby4 <felipeoliveramantero@gmail.com>
2023-10-31 09:17:52 -0700

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		for (; j < strlen(argv[i]); j++)
+		for (j = 0; j < strlen(argv[i]); j++)
 		{
 		if (isdigit(argv[i][j]) == 0)
 		{
