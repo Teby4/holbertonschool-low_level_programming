5803f277f39b041787e41b83ceccfb1001180e09
Teby4 <felipeoliveramantero@gmail.com>
2023-11-01 09:45:05 -0700

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -24,18 +24,19 @@ char *str_concat(char *s1, char *s2)
 	{
 		return(NULL);
 	}
-
+	if (s1 != '\0'){
 	for (cont1 = 0; cont1 < strlen(s1); cont1++)
 	{
 	j[cont1] = s1[cont1];
 	}
+	}
+	if (s2 != '\0'){
 	for (cont2 = 0; cont2 < strlen(s2); cont2++)
 	{
 	j[cont1] = s2[cont2];
 	cont1++;
-	}
+	}}
 	j[cont1] = '\0';
-
 	return (j);
 
 }
