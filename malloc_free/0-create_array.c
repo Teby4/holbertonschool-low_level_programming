6f9fdb82ede6c6341e2f67607a39293ae88fb75f
Teby4 <felipeoliveramantero@gmail.com>
2023-11-01 06:17:19 -0700

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char **j;
+	char *m;
+
+	m = malloc(sizeof(int));
+	if (m == '\0')
+	{
+	return ('\0');
+	}
+
+	while (size < c)
+
+}
