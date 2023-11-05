e8af95ebcddc8cde598ef01c1669982dbcaf5bff
Teby4 <felipeoliveramantero@gmail.com>
2023-11-05 10:30:30 -0800

    2-main.c

---
+++ preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - return the name of the file
+ * Return: file name
+ * description:
+ * @argc: argc
+ * @argv: argv
+ */
+
+int main(int argc, char ** argv)
+{
+    printf("%s", argv[0]);
+    return 0;
+}
