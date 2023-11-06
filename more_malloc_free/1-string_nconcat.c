396ba7aeea6b8aceb621a822bf465cdcf66f897e
Teby4 <felipeoliveramantero@gmail.com>
2023-11-05 17:07:27 -0800

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * string_nconcat - concatinate two strings
+ * Return: the two strings together
+ * Description:
+ * @s1: string1
+ * @s2: string2
+ * @n: n
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+{
+	unsigned int cont1 = 0, cont2 = 0;
+
+
+}
