db2928057d922fa6f2b39a33d876001d7f90e5e2
Teby4 <felipeoliveramantero@gmail.com>
2023-11-09 11:10:23 -0800

    dog.h

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include <dog.h>
+#include <main.h>
+#include <stdio.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog my_dog;
+my_dog.name = name;
+my_dog.age = age;
+my_dog.owner = owner;
+
+return (new_dog);
+}
