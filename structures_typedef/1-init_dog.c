ae96d17d564932c208f9db2390be3fe149c4ee16
Teby4 <felipeoliveramantero@gmail.com>
2023-11-09 11:45:18 -0800

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,6 +2,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * init_dog - 
+ * Return:
+ * @name:
+ * @age:
+ * @owner
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 d->name = name;
