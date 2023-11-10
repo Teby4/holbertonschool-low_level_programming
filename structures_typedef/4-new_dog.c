f66b8f21f9b22d8d31162c645f713ed4e467e5f5
Teby4 <felipeoliveramantero@gmail.com>
2023-11-10 06:19:43 -0800

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * new_dog - new dog
  * Return:
@@ -15,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog_ptr = (dog_t *)malloc(sizeof(dog_t));
 	if (new_dog_ptr == NULL)
 	{
-	return (NULL):
+	return (NULL);
 	}
 	new_dog_ptr->name = strdup(name);
 	new_dog_ptr->age = age;
