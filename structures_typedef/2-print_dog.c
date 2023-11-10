4608396c094f21e8d664cb2f6c0304da969cd3e5
Teby4 <felipeoliveramantero@gmail.com>
2023-11-10 05:38:43 -0800

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,7 +16,29 @@ void print_dog(struct dog *d)
 		if (d->name)
 		{
 		printf("Name: %s\n", d->name);
-		}	
+		}
+		if (d->name == NULL)
+		{
+		printf("(nil)")
+		}
+
+		if (d->age)
+                {
+                printf("Age: %.2f\n", d->age);
+                }
+                if (d->age == NULL)
+                {
+                printf("(nil)")
+                }
+
+		if (d->owner)
+                {
+                printf("Owner: %s\n", d->owner);
+                }
+                if (d->owner == NULL)
+                {
+                printf("(nil)")
+                }
 	
 	}
 
