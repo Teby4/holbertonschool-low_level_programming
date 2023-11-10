aee3bd35d05331af35ebbbb59164b653c24d2638
Teby4 <felipeoliveramantero@gmail.com>
2023-11-10 07:16:36 -0800

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -35,6 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog_ptr->owner == NULL)
 	{
 	free(new_dog_ptr->name);
+	free(new_dog_ptr->owner);
 	free(new_dog_ptr);
 	return (NULL);
 	}
