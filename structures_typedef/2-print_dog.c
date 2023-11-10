cb9d873a239b5a65d984595dfe680b9577bb9e72
Teby4 <felipeoliveramantero@gmail.com>
2023-11-10 05:32:56 -0800

    2-print_dog.c

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print dog
+ * Return:
+ * Description:
+ * @dog: dog struct
+ * @d: shortcut for dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name)
+		{
+		printf("Name: %s\n", d->name)
+		}	
+	
+	}
+
+
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -21,6 +21,6 @@ struct dog
 #define main_h
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
-
