51bb312c6c4492b7428a1b2d707d0febe553cef1
Teby4 <felipeoliveramantero@gmail.com>
2023-11-10 07:01:22 -0800

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,6 +15,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog_ptr = (dog_t *)malloc(sizeof(dog_t));
+	
 	if (new_dog_ptr == NULL)
 	{
 	return (NULL);
@@ -25,18 +26,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	free(new_dog_ptr);
 	return NULL;
 	}
-	
+
 	new_dog_ptr->age = age;
 
 	new_dog_ptr->owner = strdup(owner);
 
 	if (new_dog_ptr->owner == NULL)
-        {
+	{
 	free(new_dog_ptr->name);
-        free(new_dog_ptr);
-        return (NULL);
-        }
-	
-	return new_dog_ptr;
+	free(new_dog_ptr);
+	return (NULL);
+	}
 
+	return new_dog_ptr;
 }
