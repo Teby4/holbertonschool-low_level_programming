2098f14f2022ca14a28b56361ba24797e76af1cb
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 13:23:46 -0800

    2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * int_index - array iterator
+ * Return:
+ * @array: array
+ * @size: size of array
+ * @cmp: function
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+
+	unsigned int i;
+
+	if (size <= 0}
+	{
+	return (-1);
+	}
+
+	if (array != NULL && cmp != NULL)
+	{
+	for (i = 0; i < size; i++)
+	{
+	if (cmp(array[i]))
+	{
+	return (i);
+	}
+	}
+
+	return (-1);
+	}
+}
