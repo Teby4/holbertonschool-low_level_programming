3cfd64cc84ffcfebe86164826a0180890e89f989
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 12:49:27 -0800

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -44,7 +44,7 @@ void print_all(const char * const format, ...)
 	printf("%s", str);
 	break;
 	}
-	if (format[i + 0] != 0 && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+	if (format[i + 1] != 0 && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
 	{
 	printf(", ");
 	}
