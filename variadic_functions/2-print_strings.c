53b3f54842df80286958643c407e037d1254b496
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 11:56:24 -0800

    2-print_strings.c

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -32,7 +32,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		}
 
-		if (separator != NULL && i < n -1)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
