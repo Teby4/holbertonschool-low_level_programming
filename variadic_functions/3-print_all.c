f814e1ec292556175811f46da4e7e20c11738fb4
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 12:51:21 -0800

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,8 +13,8 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
 	char *str;
-	va_list(args);
 
+	va_list(args);
 	va_start(args, format);
 
 	while (format && format[i])
@@ -44,7 +44,6 @@ void print_all(const char * const format, ...)
 	{
 	printf(", ");
 	}
-
 	i++;
 	}
 
