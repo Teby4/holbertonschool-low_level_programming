4b54d2e1137dcac90528eeaed953c84d76ee4c6f
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 12:50:47 -0800

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,28 +13,23 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i = 0;
 	char *str;
-
 	va_list(args);
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-
 	switch (format[i])
 	{
 	case 'c':
 	printf("%c", va_arg(args, int));
 	break;
-
 	case 'i':
 	printf("%d", va_arg(args, int));
 	break;
-
 	case 'f':
 	printf("%f", va_arg(args, double));
 	break;
-
 	case 's':
 	str = va_arg(args, char*);
 	if (str == NULL)
@@ -44,7 +39,7 @@ void print_all(const char * const format, ...)
 	printf("%s", str);
 	break;
 	}
-	if (format[i + 1] != 0 && (format[i] == 'c' || format[i] == 'i' 
+	if (format[i + 1] != 0 && (format[i] == 'c' || format[i] == 'i'
 				|| format[i] == 'f' || format[i] == 's'))
 	{
 	printf(", ");
