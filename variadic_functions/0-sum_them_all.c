2ac9a032690d47d77c1fdd2346082cec0e8ef58b
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 10:35:40 -0800

    0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,8 +13,8 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int i;
-	va_list(args);
 
+	va_list(args);
 
 	if (n == 0)
 	{
@@ -23,7 +23,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 	sum += va_arg(args, int);
 	}
