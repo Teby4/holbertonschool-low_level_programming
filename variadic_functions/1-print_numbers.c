d2f663d8b4a22e632e434c0429478308972ff1c8
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 11:14:23 -0800

    1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,6 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_list args;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
@@ -24,9 +25,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
-		}	
+		}
 	}
 
-	va_end (args);
+	va_end(args);
 	printf("\n");
 }
