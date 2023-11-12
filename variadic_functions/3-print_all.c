817190199e5c94a26c80a59581641d3e4d89593d
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 12:25:07 -0800

    3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - print all
+ * Return: numbers
+ * Description:
+ * @format: format
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+
+	va_list args;
+
+	va_start(args, *)
+	while (format != NULL)
+	{
+
+	switch(format[i])
+	{
+	case c:
+	printf("%d", va_arg(args, char));
+	break;
+
+	case i:
+
+	break;
+
+	case f:
+
+	break;
+
+	case s:
+
+	break;
+	}
+	i++;
+	}
+
+	va_end(args);
+	printf("\n")
+}
