a7091bb0f0ed69ccf11175fcd3e25471279cdd9d
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 13:16:22 -0800

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,9 +14,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-	
+
+	if (array != NULL) 
+	{	
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
+	}
 	}
 }
