a3cf9d0420b7c9e3a740ca48288b3f9e865a3318
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 13:12:24 -0800

    1-array_iterator.c

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - array iterator
+ * Return:
+ * @array: array
+ * @size: size of array
+ * @action: action
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i:
+	
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] != NULL)
+		{
+		action(array[i]);
+		}
+	}
+}
