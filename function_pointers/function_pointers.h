8d57867ba3cb079e6f7e4ca64eada4c067ab81aa
Teby4 <felipeoliveramantero@gmail.com>
2023-11-12 13:06:02 -0800

    0-print_name.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,8 +1,6 @@
 #ifndef functions_pointer_h
 #define functions_pointer_h
 
-int int_index(int *array, int size, int (*cmp)(int));
-void array_iterator(int *array, size_t size, void (*action)(int));
-void print_name(char *name, void (*f)(char *));`
+void print_name(char *name, void (*f)(char *));
 
 #endif
