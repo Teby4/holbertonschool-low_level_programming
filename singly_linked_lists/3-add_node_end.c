2e071c0ca526b89ba84b2928d0a0f96fe7fbaaf2
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-21 13:51:37 -0300

    cm

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_node_ends - add node to list
+ * add_node_end - add node to list
  * Return: new node
  * @head: head of the list
  * @str: string
@@ -11,7 +11,6 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	size_t lent = 0;
 	list_t *last = *head;
-
 	list_t *new_node;
 
 	if (str == NULL)
@@ -20,38 +19,32 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	while (str[lent] != '\0')
 	{
-	lent++;
-	}
+	lent++; }
 
 	new_node = (list_t *)malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 	{
-	return (NULL);
-	}
+	return (NULL); }
 
 	new_node->str = strdup(str);
-
 	if (new_node->str == NULL)
 	{
 	free(new_node);
-	return (NULL);
-	}
-	new_node->len = lent;
-    new_node->next = NULL;
+	return (NULL); }
 
+	new_node->len = lent;
+	new_node->next = NULL;
 	if (*head == NULL)
 	{
-		*head = new_node;
-	}
+		*head = new_node; }
+
 	else
 	{
-	 while (last->next != NULL)
+	while (last->next != NULL)
 	{
-        last = last->next;
-	}
-	last->next = new_node;
-	}
-	return (new_node);
+		last = last->next; }
 
+	last->next = new_node; }
+	
+	return (new_node);
 }
