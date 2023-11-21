6727b0fe46b44eba102da544969d49e83f209751
Teby4 <felipeoliveramantero@gmail.com>
2023-11-21 06:38:16 -0800

    1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,1 +1,21 @@
+#include "lists.h"
 
+/**
+ *
+ *
+ *
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while(h != NULL)
+	{
+	h = h->next;
+	count++;
+	}
+
+	return (count++);
+}
