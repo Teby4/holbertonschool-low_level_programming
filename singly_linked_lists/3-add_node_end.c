2c167b728bad0cb64557a539449a335f446a5253
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-21 13:34:18 -0300

    commit

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * add_node - add node to list
+ * Return: new node
+ * @head: head of the list
+ * @str: string
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	size_t i = 0, lent = 0;
+
+	list_t *new_node;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+	i++;
+	lent++;
+	}
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
+	new_node->len = lent;
+	new_node->next = *head;
+	*head = new_node;
+	new_node->next = NULL;
+
+	 while (last->next != NULL)
+	{
+        last = last->next;
+	}
+	last->next = new_node;
+
+	return (new_node);
+
+}
