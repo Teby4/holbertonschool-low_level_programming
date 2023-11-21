44d03c7a22f1accb3f7cf2316b5c626efee04e07
Teby4 <felipeoliveramantero@gmail.com>
2023-11-21 06:23:08 -0800

    0-print_list.c

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,9 @@
 #define lists_h
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,5 +23,6 @@ typedef struct list_s
 
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
