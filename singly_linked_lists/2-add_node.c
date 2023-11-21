dbc227b18e9449cf460917b1737764b6120af48c
Teby4 <felipeoliveramantero@gmail.com>
2023-11-21 07:52:24 -0800

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * add_node - add node to list
@@ -16,7 +16,7 @@ list_t *add_node(list_t **head, const char *str)
 	return (NULL);
 	}
 	
-	*new_node= (list_t *)malloc(sizeof(list_t));
+	new_node= (list_t *)malloc(sizeof(list_t));
 
 	if (new_node == null)
 	{
