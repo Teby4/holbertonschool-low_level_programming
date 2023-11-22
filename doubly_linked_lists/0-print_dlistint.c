42955f2d8322e49e226b62659d4f278f0e3a8a2c
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-22 10:18:09 -0300

    print dlstint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -6,16 +6,16 @@
  * @h: h
  */
 
- size_t print_dlistint(const dlistint_t *h)
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
 	while(h != NULL)
 	{
-	 	printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
 
-	return(count);
+	return (count);
 }
