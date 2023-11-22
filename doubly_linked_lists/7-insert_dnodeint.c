29930384f1ab418ba6478e14b48694eeb81feadf
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-22 14:27:18 -0300

    TASK 7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert_dnodeint_at_index
+ * Return: dlistint
+ * @h: h
+ * @idx: idx
+ * @n: n
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+
+    
+}
