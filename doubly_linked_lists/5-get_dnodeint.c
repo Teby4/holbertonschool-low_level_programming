a361c3b00a6acd560e3202e53a8f318893f24fc3
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-22 11:50:13 -0300

    get dnodeint

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -9,6 +9,14 @@
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-
-
+    while(head && index != NULL)
+    {
+        head = head->next;
+        index--;
+    }
+    if (head && index == 0)
+    {
+		return (head);
+    }
+	return (NULL);
 }
