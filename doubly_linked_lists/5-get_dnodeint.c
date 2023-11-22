a7bc695b456a5f9dbedbcf30d865a9eba23c2a46
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-22 11:44:04 -0300

    commit

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get index
+ * Return: count
+ * @head: head
+ * @index: index
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+
+
+}
