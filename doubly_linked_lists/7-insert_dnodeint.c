e463591669d5206fa769a10cca74c5cecae9b9cf
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-23 14:25:29 -0300

    commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -18,6 +18,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (idx == 0)
+	{
+		return(add_dnodeint(h, n));
+	}
+	current_node = *h;
+	for (i = 0; i < idx -1 && current != NULL; i++)
+	{
+		current = current->next;
+	}
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
@@ -25,37 +35,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
 
-	if (idx == 0)
+	if (current_node->next != NULL)
 	{
-		new_node->next = *h;
-		if (*h != NULL)
-		{
-			(*h)->prev = new_node;
-			*h = new_node;
-			return (new_node);
-		}
-	}
-	current = *h;
-	for (i = 0; i < idx -1 && current != NULL; i++)
-	{
-		current = current->next;
+	current_node->next->prev = new_node;
+	new_node->prev = current_node;
+	new_node->next = current_node->next;
+	current_node->next = new_node;
 	}
 
-	if (current == NULL)
-	{
-		free (new_node);
-		return (NULL);
-	}
-
-	new_node->next = current->next;
-    new_node->prev = current;
-    if (current->next != NULL)
-	{
-        current->next->prev = new_node;
-    current->next = new_node;
-	}
-    return (new_node);
+	return (new_node);
 }
