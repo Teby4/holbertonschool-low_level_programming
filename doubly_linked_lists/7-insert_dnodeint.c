0dd8d9b16dd2f6fb837d04fca783f03a42590a59
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-23 14:31:51 -0300

    commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,24 +16,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 	{
-		return (NULL);
-	}
+		return (NULL);}
+
 	if (idx == 0)
 	{
-		return(add_dnodeint(h, n));
-	}
+		return (add_dnodeint(h, n));}
 	current = *h;
-	for (i = 0; i < idx -1 && current != NULL; i++)
+	for (i = 0; i < idx - 1 && current != NULL; i++)
 	{
-		current = current->next;
-	}
+		current = current->next;}
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
+		return (NULL);}
+
+	if (current == NULL)
+	{
+		free (new_node);
 		return (NULL);
 	}
-
 	new_node->n = n;
 
 	if (current->next != NULL)
@@ -41,8 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current->next->prev = new_node;
 	new_node->prev = current;
 	new_node->next = current->next;
-	current->next = new_node;
-	}
+	current->next = new_node;}
 
 	return (new_node);
 }
