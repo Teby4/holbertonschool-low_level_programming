08da35d66958e2bf73103c92fc903af91cbeb603
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-23 14:14:21 -0300

    commit

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node->n = n;
 	new_node->prev = NULL;
-	newnode->next = NULL;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
