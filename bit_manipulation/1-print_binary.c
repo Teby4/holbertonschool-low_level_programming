aaf052995c723eb238d1eb6ccf59fe8ae01cc882
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-23 11:06:03 -0300

    commit

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,4 +1,4 @@
-#include <stddef.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,5 +10,5 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 		print_binary(n >> 1);
 
-	putchar((n & 1) + '0');
+	putchar((n & 1) ? '1' : '0');
 }
