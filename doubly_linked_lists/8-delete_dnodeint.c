5b6991b81bf5dd70a0fb4e57033aab743e4ce687
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-24 11:40:04 -0300

    commit

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -28,7 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (-1);
 		}
 
-		if (!index && current_node)
+		if (!index && current)
 		{
 			if (current_node->next)
 				current_node->next->prev = current_node->prev;
