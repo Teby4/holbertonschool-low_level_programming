42d02e697a68b54a2bb7bb13da427e926eca2334
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-24 11:45:34 -0300

    commit

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * delete_dnodeint_at_index - delete nodes till index
- * Return 1 if succed
+ * Return: 1 if succed
  * @head: head of the nodes
  * @index: number to delete
  */
@@ -10,37 +10,37 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = *head;
-	unsigned int i;
+    unsigned int i;
 
-	if (!head)
-	{
-		return (-1);
-	}
+    if (!head || !(*head))
+    {
+        return (-1);
+    }
 
-	if (head)
-	{
-		for (i = 0; i < index; i++)
-		{
-			current = current->next;
-		}
-		if (index)
-		{
-			return (-1);
-		}
+    for (i = 0; i < index && current; i++)
+    {
+        current = current->next;
+    }
 
-		if (!index && current)
-		{
-			if (current->next)
-				current->next->prev = current->prev;
+    if (i < index || !current)
+    {
+        return (-1);
+    }
 
-			if (current->prev)
-				current->prev->next = current->next;
-			else
-				*head = current->next;
+    if (current->next)
+    {
+        current->next->prev = current->prev;
+    }
 
-			free(current);
-			return (1);
-		}
-	}
-	return (-1);
+    if (current->prev)
+    {
+        current->prev->next = current->next;
+    }
+    else
+    {
+        *head = current->next;
+    }
+
+    free(current);
+    return (1);
 }
