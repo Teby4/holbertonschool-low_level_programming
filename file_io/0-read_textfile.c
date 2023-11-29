0370f4f5ed2747ffd9efe65a706c5208f5784c40
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-29 10:15:51 -0300

    commit

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,44 +14,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return (0);
-	}
+		return (0);}
 
 	fd = open(filename, O_RDWR);
 
 	if (fd == -1)
 	{
-		return(0);
-	}
+		return (0);}
 
 	buffer = (char *)malloc(letters + 1);
-
 	if (buffer == NULL)
 	{
 		int close(int fd);
-		return (0);
-	}
+		return (0);}
 
 	bytesRead = read(fd, buffer, letters);
-	close (fd);
+	close(fd);
 
 	if (bytesRead < 0)
 	{
-		free (buffer);
-		return (0);
-	}
+		free(buffer);
+		return (0);}
 
 	buffer[bytesRead] = '\0';
 	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
-
-	free (buffer);
+	free(buffer);
 
 	if (bytesWritten == bytesRead)
 	{
-		return (bytesRead);
-	}
+		return (bytesRead);}
 	else
 	{
-		return (0);
-	}
+		return (0);}
 }
