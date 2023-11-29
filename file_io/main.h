fbe644fa71316930242aa2cab5ada9b4d9e36b92
Felipe Olivera <135641220+Teby4@users.noreply.github.com>
2023-11-29 09:39:03 -0300

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,8 +1,9 @@
 #ifndef main_h
 #define main_h
 
-#include
-#include
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
