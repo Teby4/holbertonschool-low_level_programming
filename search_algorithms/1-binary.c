4e48552fcafecc91e20e72f29bff06b8b4f9bc29
root <felipeoliveramantero@gmail.com>
2024-01-21 01:42:07 +0000

    1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 	if (array == 0)
 		return (-1);
 
-	while(low <= high)
+	while (low <= high)
 	{
 		middle = low + (high - low) / 2;
 		target = array[middle];
@@ -31,7 +31,7 @@ int binary_search(int *array, size_t size, int value)
 		{
 			high = (middle - 1);
 		}
-		else 
+		else
 			return (middle);
 	}
 	return (-1);
