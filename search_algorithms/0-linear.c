6d529d4ebc77a2a296b6df06c6af96d2f6237312
root <felipeoliveramantero@gmail.com>
2024-01-21 00:22:34 +0000

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -14,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (j = 0; j <= size; j++)
 	{
-		printf("Value checked array [%lu] = [%d]\n", j, array[j]);
+		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
 		if (value == array[j])
 		{
 			return (j);
